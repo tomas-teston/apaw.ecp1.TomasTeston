1a958ed9e6dfce4ab0b5fb4b520cea1e49481614
Tomas Muñoz Testón <tomini18@hotmail.com>
2018-10-03 21:44:23 +0200

    Created Component Pattern in "Jefe" Entity and Created Test.

--- src/main/java/es/upm/miw/apaw/ecp1/junit/Jefe.java
+++ src/main/java/es/upm/miw/apaw/ecp1/junit/Jefe.java
@@ -1,14 +1,15 @@
 package es.upm.miw.apaw.ecp1.junit;
 
-public class Jefe {
+public class Jefe extends JefeComponent {
 
     private int id;
     private String nombre;
     private int telefono;
 
-    public Jefe() {
+    public Jefe(String name, int id, String nombre, String telefono) {
+        super(name);
         this.id = 0;
-        this.nombre = "";
+        this.nombre = nombre;
         this.telefono = 0;
     }
 
@@ -24,4 +25,19 @@ public class Jefe {
         return telefono;
     }
 
+    @Override public void add(JefeComponent jefeComponent) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+
+    @Override public void remove(JefeComponent jefeComponent) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+
+    @Override public boolean isComposite() {
+        return false;
+    }
+
+    @Override public String view() {
+        return Integer.toString(this.getId());
+    }
 }
