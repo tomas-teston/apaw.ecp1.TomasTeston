92e4a676cc2c3b97f6df5f1b7c16ded2a392808e
Tomas Muñoz Testón <tomini18@hotmail.com>
2018-10-07 17:22:08 +0200

    bug fixed jefe Composite.

--- src/main/java/es/upm/miw/apaw/ecp1/junit/Jefe.java
+++ src/main/java/es/upm/miw/apaw/ecp1/junit/Jefe.java
@@ -1,13 +1,12 @@
 package es.upm.miw.apaw.ecp1.junit;
 
-public class Jefe extends JefeComponent {
+public class Jefe{
 
     private int id;
     private String nombre;
     private int telefono;
 
-    public Jefe(String name, int id, String nombre, String telefono) {
-        super(name);
+    public Jefe(int id, String nombre, String telefono) {
         this.id = 0;
         this.nombre = nombre;
         this.telefono = 0;
@@ -23,21 +22,5 @@ public class Jefe extends JefeComponent {
 
     public int getTelefono() {
         return telefono;
-    }
-
-    @Override public void add(JefeComponent jefeComponent) {
-        throw new UnsupportedOperationException("Operación no soportada");
-    }
-
-    @Override public void remove(JefeComponent jefeComponent) {
-        throw new UnsupportedOperationException("Operación no soportada");
-    }
-
-    @Override public boolean isComposite() {
-        return false;
-    }
-
-    @Override public String view() {
-        return Integer.toString(this.getId());
     }
 }
---
+++ src/main/java/es/upm/miw/apaw/ecp1/junit/JefeLeaf.java
@@ -0,0 +1,31 @@
+package es.upm.miw.apaw.ecp1.junit;
+
+public class JefeLeaf extends JefeComponent {
+
+    private Jefe jefe;
+
+    public JefeLeaf(String name, Jefe jefe) {
+        super(name);
+        this.jefe = jefe;
+    }
+
+    public Jefe getJefe() {
+        return this.jefe;
+    }
+
+    @Override public void add(JefeComponent jefeComponent) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+
+    @Override public void remove(JefeComponent jefeComponent) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+
+    @Override public boolean isComposite() {
+        return false;
+    }
+
+    @Override public String view() {
+        return Integer.toString(this.jefe.getId());
+    }
+}
